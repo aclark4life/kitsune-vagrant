1c884f28e0ca053124ba552f36cd50698b414d17
Alex Clark <aclark@aclark.net>
2011-09-20 13:47:40 -0400

    Start adding commands

--- manifests/kitsune.pp
+++ manifests/kitsune.pp
@@ -6,5 +6,13 @@ File { owner => 0, group => 0, mode => 0644 }
 
 file { '/etc/motd':
   content => "Welcome to your Vagrant-built virtual machine!
-              Managed by Puppet.\n"
+              Managed by Puppet. Featuring kitsune.\n"
+}
+
+exec { "update":
+    command => "sudo aptitude update",
+}
+
+exec { "upgrade":
+    command => "sudo aptitude safe-upgrade",
 }
