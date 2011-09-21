a773e8811f12a0905883d256500fe3e2cdf73d64
Alex Clark <aclark@aclark.net>
2011-09-20 20:17:20 -0400

    Add deps

--- manifests/kitsune.pp
+++ manifests/kitsune.pp
@@ -9,14 +9,16 @@ file { '/etc/motd':
               Managed by Puppet. Featuring kitsune.\n"
 }
 
-exec { "updates":
-    command => "sudo aptitude update;
-                sudo aptitude -y safe-upgrade",
-    path => "/usr/bin",
-}
+#exec { "updates":
+#    command => "sudo aptitude update;
+#                sudo aptitude -y safe-upgrade",
+#    path => "/usr/bin",
+#}
 
 package { "git-core": ensure => "installed" }
 package { "libmysqlclient-dev": ensure => "installed" }
+package { "libxml2-dev": ensure => "installed" }
+package { "libxslt-dev": ensure => "installed" }
 package { "mysql-server":   ensure => "installed" }
 package { "python2.6":   ensure => "installed" }
 package { "python2.6-dev":   ensure => "installed" }
@@ -26,7 +28,6 @@ exec { "the_rest":
     command => "sudo easy_install-2.6 pip;
                 cd /home/vagrant;
                 git clone --recursive git://github.com/aclark4life/kitsune.git;
-                sudo chown -R vagrant:vagrant /home/vagrant/kitsune;
                 cd /home/vagrant/kitsune;
                 sudo pip install -r requirements/compiled.txt;
                 git submodule update --init --recursive;",
